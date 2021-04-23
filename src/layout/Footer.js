51ea49956fe566833a5906c63617f4bdeb4e31d6
tirth-2001 <tirthgpatel.27@gmail.com>
2021-04-24 01:07:57 +0530

    CICD Test

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -3,8 +3,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-info text-center pt-3 pb-2 mt-5 sticky-bottom white">
-      Created by Rushi Patel.
+    <footer className="bg-secondary text-center pt-3 pb-2 mt-5 sticky-bottom text-white">
+      Created by Rushi Patel ☺️
     </footer>
   );
 };
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Navbar color="info" light>
+    <Navbar color="secondary" light>
       <NavbarBrand tag={Link} to="/" className="text-white">
         My Contact App
       </NavbarBrand>
