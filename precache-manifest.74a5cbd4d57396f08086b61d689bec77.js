ff5e8188fed558015fb8dc7c4f48fba8e855b4fe
tirth-2001 <tirthgpatel.27@gmail.com>
2021-04-23 23:53:03 +0530

    initial commit

---
+++ precache-manifest.74a5cbd4d57396f08086b61d689bec77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8381d912d49a9be5ab181733d5b015c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "959e43d16952cc476397",
+    "url": "/static/css/2.e5b99fdb.chunk.css"
+  },
+  {
+    "revision": "6330707fa1a87a708e40",
+    "url": "/static/css/main.bcb5a572.chunk.css"
+  },
+  {
+    "revision": "959e43d16952cc476397",
+    "url": "/static/js/2.3dde4b88.chunk.js"
+  },
+  {
+    "revision": "2520f30cb4e102284a8f5fa3a821a3bf",
+    "url": "/static/js/2.3dde4b88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6330707fa1a87a708e40",
+    "url": "/static/js/main.7ea59f85.chunk.js"
+  },
+  {
+    "revision": "c7cb78d65c7ae0baf90a",
+    "url": "/static/js/runtime-main.c1fcbbc2.js"
+  }
+]);
