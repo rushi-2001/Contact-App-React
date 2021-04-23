0f04e097028cf2cedf45e0adf0ca9ad78834c6ce
tirth-2001 <tirthgpatel.27@gmail.com>
2021-04-24 00:45:42 +0530

    react-v1

---
+++ src/utils/config.js
@@ -0,0 +1,20 @@
+//TODO: add firebase configuration and export it
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyAAPq6g8G-NSOZagGQTt_4uqmD4drx5OSk",
+  authDomain: "github-app-c5037.firebaseapp.com",
+  databaseURL: "https://github-app-c5037-default-rtdb.firebaseio.com",
+  projectId: "github-app-c5037",
+  storageBucket: "github-app-c5037.appspot.com",
+  messagingSenderId: "137758569905",
+  appId: "1:137758569905:web:af63f2ed6ae0b82ae3b990",
+  measurementId: "G-9PENDHGFBQ",
+};
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true,
+};
